138d847cda6488acc9970f00a1a617e7115e74d1
carloslima <carloslima@brq.com>
2021-11-19 17:41:38 -0300

    alterando fluxo

--- app/src/main/java/com/chslcompany/country/view/activity/HomeActivity.kt
+++ app/src/main/java/com/chslcompany/country/view/activity/HomeActivity.kt
@@ -3,6 +3,7 @@ package com.chslcompany.country.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.chslcompany.country.R
@@ -14,17 +15,19 @@ class HomeActivity : AppCompatActivity() {
     private val binding : ActivityHomeBinding get() = _binding!!
     private lateinit var navController : NavController
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         _binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-        if (navHostFragment != null) {
-            navController.let {  navHostFragment.findNavController() }
-        }
+                as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        //if (navHostFragment != null) {
+           // navController.let {  navHostFragment.findNavController() }
+        //}
 
         binding.bottomNavigation.setupWithNavController(navController)
         binding.bottomNavigation.setOnItemReselectedListener {
